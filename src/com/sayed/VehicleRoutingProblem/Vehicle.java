701442cac32842d20216d2ed513ac3e2ecef57d7
Sheikh Sayed <sksayed@live.com>
2020-09-10 19:52:15 +0600

    project succesfully ran

---
+++ src/com/sayed/VehicleRoutingProblem/Vehicle.java
@@ -0,0 +1,18 @@
+package com.sayed.VehicleRoutingProblem;
+
+public class Vehicle {
+
+	int capacity;
+	
+	public Vehicle(int cap){
+		this.capacity = cap;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehicle [capacity=" + capacity + "]";
+	}
+	
+	
+	
+}
